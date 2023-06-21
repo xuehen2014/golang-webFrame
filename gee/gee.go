a4be891accac4555cae75ccb057be166cb14541f
zhangjiulong-ps <zhangjiulong-ps@360.cn>
2023-06-21 10:53:06 +0800

    days1

--- gee/gee.go
+++ gee/gee.go
@@ -1,3 +1,46 @@
 package gee
 
+import (
+	"fmt"
+	"net/http"
+)
 
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+var _ http.Handler = &Engine{}
+
+//Engine implement the interface of ServeHTTP
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := engine.router[key]; ok { //寻找路由
+		handler(w, req)
+	} else {
+		_, _ = fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
+}
+
+//添加路由
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	engine.router[key] = handler
+}
+
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+func (engine *Engine) Run(addr string) error {
+	return http.ListenAndServe(addr, engine)
+}
--- main.go
+++ main.go
@@ -1,3 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"github.com/xuehen2014/golang-webFrame/gee"
+	"net/http"
+)
 
+func main() {
+	r := gee.New()
+	r.GET("/", func(writer http.ResponseWriter, request *http.Request) {
+		_, _ = fmt.Fprintf(writer, "URL.PATH = %q \n", request.URL.Path)
+	})
+	r.GET("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		for k, v := range request.Header {
+			_, _ = fmt.Fprintf(writer, "Header[%q] = %q\n", k, v)
+		}
+	})
+	_ = r.Run(":9999")
+}
